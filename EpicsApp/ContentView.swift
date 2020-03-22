d92d9babbaa1c96781429a9c0815cb7f51eeac01
Carlo Forrest Dormiendo <carloForrest@users.noreply.github.com>
2020-03-22 16:19:02 -0400

    Initial Commit

---
+++ EpicsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EpicsApp
+//
+//  Created by Carlo Forrest Dormiendo on 2020-03-22.
+//  Copyright © 2020 Carlo Forrest Dormiendo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
